0c07c6d97bacb94a6f04678543437003643a6a85
Christian Yarwood <christianyarwood@gmail.com>
2025-02-26 11:59:45 +0000

    Make index with 1 container

---
+++ script.js
@@ -0,0 +1,29 @@
+const gridContainer = document.querySelector('.grid-container');
+
+/* I tried writing a dynamic grid that changes with screen size.
+I managed to dynamically change the container size, but the gris squares stay the same
+size when resizing the screen
+
+function makeGrid(size) {
+    const adjustedSize = (gridContainer.getBoundingClientRect().height) / size;
+    for (let i = 0; (i < size * size); i++) {
+        const square = document.createElement('div');
+        square.style.height = adjustedSize + 'px';
+        square.style.width = adjustedSize + 'px';
+        square.classList.add('grid-square');
+        gridContainer.appendChild(square);
+    }
+}
+*/
+
+function makeGrid(size) {
+
+    const adjustedSize = (gridContainer.getBoundingClientRect().height) / size;
+    for (let i = 0; (i < size * size); i++) {
+        const square = document.createElement('div');
+        square.style.height = adjustedSize + 'px';
+        square.style.width = adjustedSize + 'px';
+        square.classList.add('grid-square');
+        gridContainer.appendChild(square);
+    }
+}
