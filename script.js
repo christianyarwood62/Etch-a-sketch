2aa0a6f9b00718d76a811a6d1e0d2602bbf5ed56
Christian Yarwood <christianyarwood@gmail.com>
2025-02-28 09:26:07 +0000

    Make grid container appear with function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('.grid-container');
+const buttonContainer = document.querySelector('.button-container');
 
 /* I tried writing a dynamic grid that changes with screen size.
 I managed to dynamically change the container size, but the gris squares stay the same
@@ -15,14 +16,21 @@ function makeGrid(size) {
     }
 }
 */
+// Create a button, add it to a class and add text
+startButton = document.createElement('button');
+startButton.classList.add('start-button');
+startButton.textContent = 'test';
+buttonContainer.appendChild(startButton);
 
 function makeGrid(size) {
-
+    gridContainer.style.width = '740px';
+    gridContainer.style.height = '740px';
     const adjustedSize = (gridContainer.getBoundingClientRect().height) / size;
     for (let i = 0; (i < size * size); i++) {
         const square = document.createElement('div');
         square.style.height = adjustedSize + 'px';
         square.style.width = adjustedSize + 'px';
+        
         square.classList.add('grid-square');
         gridContainer.appendChild(square);
     }
