63a418f45e6dc6afbbe2f18fc974effae04bdc82
Christian Yarwood <christianyarwood@gmail.com>
2025-02-28 20:49:56 +0000

    Add opacity increase to boxes

--- script.js
+++ script.js
@@ -30,18 +30,16 @@ makeGrid(10);
 function makeGrid(size) {
     gridContainer.style.width = '400px';
     gridContainer.style.height = '400px';
+    const numOfSquares = size * size;
     const adjustedSize = (gridContainer.getBoundingClientRect().height) / size;
-    for (let i = 0; (i < size * size); i++) {
+    for (let i = 0; (i < numOfSquares); i++) {
         const square = document.createElement('div');
         square.style.height = adjustedSize + 'px';
         square.style.width = adjustedSize + 'px';
         square.classList.add('grid-square');
         gridContainer.appendChild(square);
-        square.addEventListener('mouseover', (e) => {
-            const randomColor = Math.floor(Math.random()*16777215).toString(16);
-            e.target.style.backgroundColor = '#' + randomColor;
-        })
     }
+    addEventListenersToSquares();
 }
 
 // Empty the current grid
@@ -66,3 +64,20 @@ startButton.addEventListener('click', () => {
         alert('Please enter a valid number')
     }
 })
+
+function addEventListenersToSquares() {
+    let gridBoxes = document.querySelectorAll('.grid-square');
+    gridBoxes.forEach(box => {
+        let opacity = 0;
+        box.addEventListener('mouseenter', () => {
+            box.style.backgroundColor = 'red';
+            opacity += 15;
+            box.style.opacity = `${opacity}%`;
+        })
+    });
+}
+
+    // sqr.addEventListener('mouseover', (e) => {
+    //     const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    //     e.target.style.backgroundColor = '#' + randomColor;
+    // })
